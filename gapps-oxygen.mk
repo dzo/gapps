70b2ff9b15561fd27512111286f6d8b524192056
Adam Green <greena88@gmail.com>
2011-05-21 01:47:56 +0100

    Initial commit

---
+++ gapps-oxygen.mk
@@ -0,0 +1,22 @@
+# Google Apps
+PRODUCT_COPY_FILES += \
+    vendor/devnull/gapps/system/app/FOTAKill.apk:system/app/FOTAKill.apk \
+    vendor/devnull/gapps/system/app/GenieWidget.apk:system/app/GenieWidget.apk \
+    vendor/devnull/gapps/system/app/GoogleBackupTransport.apk:system/app/GoogleBackupTransport.apk \
+    vendor/devnull/gapps/system/app/GoogleCalendarSyncAdapter.apk:system/app/GoogleCalendarSyncAdapter.apk \
+    vendor/devnull/gapps/system/app/GoogleContactsSyncAdapter.apk:system/app/GoogleContactsSyncAdapter.apk \
+    vendor/devnull/gapps/system/app/GoogleFeedback.apk:system/app/GoogleFeedback.apk \
+    vendor/devnull/gapps/system/app/GooglePartnerSetup.apk:system/app/GooglePartnerSetup.apk \
+    vendor/devnull/gapps/system/app/GoogleQuickSearchBox.apk:system/app/GoogleQuickSearchBox.apk \
+    vendor/devnull/gapps/system/app/GoogleServicesFramework.apk:system/app/GoogleServicesFramework.apk \
+    vendor/devnull/gapps/system/app/LatinImeTutorial.apk:system/app/LatinImeTutorial.apk \
+    vendor/devnull/gapps/system/app/MarketUpdater.apk:system/app/MarketUpdater.apk \
+    vendor/devnull/gapps/system/app/MediaUploader.apk:system/app/MediaUploader.apk \
+    vendor/devnull/gapps/system/app/NetworkLocation.apk:system/app/NetworkLocation.apk \
+    vendor/devnull/gapps/system/app/OneTimeInitializer.apk:system/app/OneTimeInitializer.apk \
+    vendor/devnull/gapps/system/app/SetupWizard.apk:system/app/SetupWizard.apk \
+    vendor/devnull/gapps/system/app/Talk.apk:system/app/Talk.apk \
+    vendor/devnull/gapps/system/app/Vending.apk:system/app/Vending.apk \
+    vendor/devnull/gapps/system/etc/permissions/com.google.android.maps.xml:system/etc/permissions/com.google.android.maps.xml \
+    vendor/devnull/gapps/system/etc/permissions/features.xml:system/etc/permissions/features.xml \
+    vendor/devnull/gapps/system/framework/com.google.android.maps.jar:system/framework/com.google.android.maps.jar
